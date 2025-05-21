cd5b4acd5fc460f3720e821890566e6029aa9daf
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-21 08:39:16 +0530

    add file

---
+++ footer.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+
+    <footer class="text-white pb-5 pt-4" style="background: #ADBBDA;">
+
+        <div class="col-12 text-md-start">
+            <div class="row text-sm-center text-md-start">
+
+                <div class="col-12 col-md-3 col-lg-3 mx-auto mt-3">
+                    <h5 class="text-uppercase text-white mb-4">Online Fashion Store</h5>
+                    <p>We believe fashion is a way of life.Shop with us and
+                        express your unique style!</p>
+                </div>
+
+                <div class="col-12 col-md-3 col-lg-2 mx-auto mt-3">
+                    <p>Copyright &copy; 2024 Online Fashion Store All Rights Reserved.</p>
+                </div>
+
+                <div class="col-12 col-md-2 col-lg-2 mx-auto mt-3">
+                    <div class="text-sm-center text-md-start">
+
+                        <ul class="list-unstyled list-inline">
+
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-facebook" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-whatsapp" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-linkedin" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-youtube" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 col-lg-3 mx-auto mt-3">
+
+                    <h5 class="text-uppercase text-white mb-4">Contact</h5>
+
+                    <p><i class="bi bi-house-fill"></i> Kadawatha, Sri Lanka</p>
+                    <p><i class="bi bi-at"></i> familyhiru@gmail.com</p>
+                    <p><i class="bi bi-telephone-fill"></i> +94 787 957 300</p>
+
+                </div>
+
+                <hr class="mb-1" style="color: black;" />
+
+            </div>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
