316fe9307cdd1d25cb6a189fceb7af99b6afd6b3
dreadlord-sedai <dahamifabbio@gmail.com>
2025-05-21 08:40:45 +0530

    add file

---
+++ about.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About us | Online Fashion Store</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="icon" href="resources/logo design/fashin_logo.png" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+</head>
+
+<body style="overflow-x: hidden;">
+
+    <div class="mt-4" style="margin-left: 20px;">
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item fw-bold"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">About</li>
+            </ol>
+        </nav>
+    </div>
+
+
+
+    <div class="aboutus1">
+        <h1>About Us</h1>
+        <p>
+            Welcome to Online Fashion Store,
+            where fashion meets passion! Founded with a love for style and a mission to empower
+            individuality, we curate collections that celebrate every personality, occasion, and trend.
+            From chic everyday essentials to bold statement pieces, our fashion-forward designs are crafted
+            with quality and attention to detail in mind.
+        </p>
+    </div>
+
+    <div class="aboutus2">
+        <section class="aboutus3">
+            <div class="aboutus4">
+                <img src="resources/logo design/fashin_logo.png">
+            </div>
+
+            <div class="aboutus5">
+                <h2>Online Fashion Store</h2>
+                <p>
+                    At Online Fashion Store, we believe fashion is a form of self-expression.
+                    That is why our range includes everything from timeless classics to the latest trends, ensuring
+                    there is something for everyone. Whether you are dressing for work, play, or something in between,
+                    our pieces will help you look and feel your best.
+
+                    Our commitment goes beyond just clothes.
+                    We are dedicated to creating a seamless shopping experience,
+                    from easy browsing and fast shipping to top-notch customer service.
+                    Our team is always here to help you find the perfect look or answer any questions you might
+                    have.
+                    Thank you for choosing Online Fashion Store as your go-to fashion destination.
+                    Together, let's make every outfit a statement !
+                </p>
+            </div>
+        </section>
+    </div>
+
+    <!--footer--->
+    <?php
+    include "footer.php";
+    ?>
+</body>
+
+</html>
